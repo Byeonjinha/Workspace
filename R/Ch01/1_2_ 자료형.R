491b9f92cf05e38603ccead70029a28618530a10
Byeonjinha <jinhaday@gmail.com>
2021-09-27 12:18:18 +0900

    2021-09-27

---
+++ R/Ch01/1_2_ 자료형.R
@@ -0,0 +1,34 @@
+# 날짜 : 2021-09-27
+# 이름 : 변진하
+# 내용 : R 설치와 개요 - 변수실습하기 교재 p41
+
+#숫자형 (정수, 실수)
+num1 <- 1
+num2 <- 2
+num3 <- 3.14
+num1
+num2; num3
+
+ 논리형
+bool1 <- TRUE
+bool2 <- FALSE
+bool1
+bool2
+
+#문자형(문자, 문자열)
+str1 <- "Hello"
+str2 <- "안녕"
+str1
+str2
+
+#특수형
+var1 <-NULL # 초기값을 의미
+var2 <- NA  # 결측값, Not available
+var3 <- NaN #수학적으로 정의가 불가능한 값, Not a Number
+var1
+var2
+var3
+
+
+
+
---
+++ R/Ch02/2_6_String 문자처리.R
@@ -0,0 +1,19 @@
+#날짜 : 2021-09-27
+#이름 : 변진하
+#내용 : 데이터 유형과 구조 - 문자열 처리 실습하기 교재 p84
+
+#R 패키지 설치
+#install.packages('stringr')
+
+#R 패키지 로드
+library(stringr)
+
+#문자열
+str <- 'hello world'
+str 
+
+#문자열 길이
+str_length(str)
+
+#문자열 자르기
+str_sub(str,1,5)
